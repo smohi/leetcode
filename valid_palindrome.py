ba655ff8381383d83b5064a8ed3a167f73789e08
smohi <smohi.wd@gmail.com>
2024-10-07 20:14:29 +0900

    valid palindrome

---
+++ valid_palindrome.py
@@ -0,0 +1,6 @@
+def isPalindrome(self, s: str) -> bool:
+        filtered = ''
+        for c in s:
+            if c.isalnum():
+                filtered += c.lower()
+        return filtered == filtered[::-1]
