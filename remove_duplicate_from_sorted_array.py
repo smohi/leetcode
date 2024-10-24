d0648da54054e90d85c56c6bb9da26498a37c15a
smohi <smohi.wd@gmail.com>
2024-10-24 21:53:56 +0900

    remove duplicate from a sorted array problem

---
+++ remove_duplicate_from_sorted_array.py
@@ -0,0 +1,11 @@
+if not nums:
+            return 0
+        
+        j = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[j - 1]:
+                nums[j] = nums[i]
+                j += 1
+        
+        return j
